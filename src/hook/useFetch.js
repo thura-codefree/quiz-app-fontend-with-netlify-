d39c9c66034a5065b93e0c1cb7114752a73f809d
kyawthura <thurasoft@gmail.com>
2022-09-17 23:21:28 +0630

    first commit

---
+++ src/hook/useFetch.js
@@ -0,0 +1,16 @@
+import  {useState,useEffect} from 'react';
+import axios from 'axios';
+const useFetc = () =>{
+    const [loader,setLoader] = useState(true);
+    const [quiz, setQuiz] = useState([]);
+    useEffect(()=>{
+        axios.get("https://thuraquizapi.herokuapp.com/api/quiz").then((res)=>{
+            const data = res.data;
+            setQuiz(data);
+            setLoader(false);
+            
+        })
+    }, []);
+    return {loader,quiz}
+}
+export default useFetc;
